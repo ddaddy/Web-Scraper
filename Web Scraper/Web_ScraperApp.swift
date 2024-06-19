61e224655800596ae49726a6b4748cf21066a6ea
Darren Jones <1425808+ddaddy@users.noreply.github.com>
2024-06-19 09:41:00 +0200

    Initial Commit

---
+++ Web Scraper/Web_ScraperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Web_ScraperApp.swift
+//  Web Scraper
+//
+//  Created by Darren Jones on 19/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Web_ScraperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
